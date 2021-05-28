1141133facfada9beb27f248bc620f6003e8dcf9
Marina <mmolostvova@gmail.com>
2021-05-28 20:51:36 +0300

    implement assert

--- __tests__/capitalize.test.js
+++ __tests__/capitalize.test.js
@@ -1,11 +1,8 @@
+import { strict as assert } from 'assert';
+
 import { capitalize } from '../src/capitalize.js'
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepStrictEqual(capitalize('hello'), 'Hello');
+assert.deepStrictEqual(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
